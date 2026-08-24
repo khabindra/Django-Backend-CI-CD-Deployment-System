d1308cebf5afad8c75b9a877ef6f31dd3aab102f
khabindra tamang <pujakhabi78@gmail.com>
2026-08-24 15:43:03 +0545

    just added to test migration check fail in github actions ci

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    task_name = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
